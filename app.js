c11e0463e9a73d630876ff4b3142443af4475873
Kyle Bulloch & Bryan Borgeson <student@epicodus.com>
2015-04-28 11:28:29 -0700

    Make hirebox into a ui-view

--- app.js
+++ app.js
@@ -1,5 +1,16 @@
 var freemancer = angular.module("freemancer", ["ui.router"]);
 
+freemancer.config(function($stateProvider) {
+  $stateProvider.state("hirebox", {
+    url: "",
+    views: {
+      "hirebox": {
+        templateUrl: "partials/hirebox.html"
+      }
+    }
+  });
+});
+
 freemancer.directive("navbar", function() {
   return {
     restrict: "E",
